169edde28e65788468f05315003649e4b99d76d3
Wayne Jared <waynejared@gmail.com>
2022-10-28 16:37:46 -0500

    last files to update

--- sdk/Event/Unsubscribe_auto.go
+++ sdk/Event/Unsubscribe_auto.go
@@ -7,9 +7,9 @@ package event
 import (
 	"context"
 	"github.com/juju/errors"
-	"github.com/use-go/onvif"
-	"github.com/use-go/onvif/sdk"
-	"github.com/use-go/onvif/event"
+	"github.com/waynejared/onvif"
+	"github.com/waynejared/onvif/sdk"
+	"github.com/waynejared/onvif/event"
 )
 
 // Call_Unsubscribe forwards the call to dev.CallMethod() then parses the payload of the reply as a UnsubscribeResponse.
